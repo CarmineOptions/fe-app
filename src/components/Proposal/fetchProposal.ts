ea2f8f949de68d86f8da265fd35a64f4d424f5a0
DaveVodrazka <dave.vodrazka@gmail.com>
2023-12-29 09:05:48 +0100

    feat: remove prop 44

--- src/components/Proposal/fetchProposal.ts
+++ src/components/Proposal/fetchProposal.ts
@@ -2,9 +2,9 @@ import { Proposal } from "../../types/proposal";
 
 export const activeProposal: Proposal[] = [
   // Example proposals:
-  {
-    id: 44,
-    discordLink:
-      "https://discord.com/channels/969228248552706078/1187501347684220958",
-  },
+  // {
+  //   id: 44,
+  //   discordLink:
+  //     "https://discord.com/channels/969228248552706078/1187501347684220958",
+  // },
 ];
