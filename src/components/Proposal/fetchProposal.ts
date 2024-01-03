312af99ed30ca6294c88d0423231ac0d913a80d2
DaveVodrazka <dave.vodrazka@gmail.com>
2024-01-03 20:08:33 +0100

    fix: prop 45 correct discord link

--- src/components/Proposal/fetchProposal.ts
+++ src/components/Proposal/fetchProposal.ts
@@ -4,6 +4,6 @@ export const activeProposal: Proposal[] = [
   {
     id: 45,
     discordLink:
-      "https://discord.com/channels/969228248552706078/1192044317179658250/1192044319729786890",
+      "https://discord.com/channels/969228248552706078/1192044317179658250",
   },
 ];
