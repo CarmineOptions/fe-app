05dc329d17d393d54493fcc88e1a0681b7eecb25
DaveVodrazka <dave.vodrazka@gmail.com>
2024-01-03 11:27:11 +0100

    chore: add discord link to prop 45

--- src/components/Proposal/fetchProposal.ts
+++ src/components/Proposal/fetchProposal.ts
@@ -1,11 +1,9 @@
 import { Proposal } from "../../types/proposal";
 
 export const activeProposal: Proposal[] = [
-  // Example proposals:
-  // {
-  //   id: 44,
-  //   discordLink:
-  //     "https://discord.com/channels/969228248552706078/1187501347684220958",
-  // },
-  { id: 45 },
+  {
+    id: 45,
+    discordLink:
+      "https://discord.com/channels/969228248552706078/1192044317179658250/1192044319729786890",
+  },
 ];
