96de6d79dacbcb4de4e03cd6ed9f94770b0be3a0
DaveVodrazka <dave.vodrazka@gmail.com>
2024-09-03 18:23:08 +0700

    fix: mobile horizontal scroll

--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -4,8 +4,6 @@ import { Layout } from "../components/Layout";
 import { Leaderboard } from "../components/Points";
 import { Helmet } from "react-helmet";
 
-import styles from "./leaderboard.module.css";
-
 const LeaderboardPage = () => {
   useEffect(() => {
     document.title = "Points | Carmine Finance";
@@ -17,7 +15,7 @@ const LeaderboardPage = () => {
         <title>Leaderboard | Carmine Options AMM</title>
         <meta name="description" content="Earn points to move up the ladder!" />
       </Helmet>
-      <div className={styles.container}>
+      <div className="gapcolumn">
         <h1>Carmine Points Program</h1>
         <p>Elevate Your Status, Enhance Your Rewards</p>
         <p>Season 2 is now live!</p>
