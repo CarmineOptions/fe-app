cf8a0fb60ace45b79a20e3c8427a6ebf75ed2d6f
DaveVodrazka <dave.vodrazka@gmail.com>
2024-09-05 15:39:51 +0700

    fix: badge and price protect

--- src/components/TradeTable/TradeTable.tsx
+++ src/components/TradeTable/TradeTable.tsx
@@ -86,7 +86,7 @@ export const TradeTable = () => {
             },
             ".css-1ly9a1d-MuiSelect-select-MuiInputBase-input-MuiOutlinedInput-input":
               { padding: 0 },
-            width: "208px",
+            width: "270px",
           }}
         >
           <MenuItem value={PairKey.STRK_USDC}>
