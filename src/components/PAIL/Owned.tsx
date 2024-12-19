d6415b477cb3cb05570f5d2ea79f13001ae87102
DaveVodrazka <dave.vodrazka@gmail.com>
2024-12-19 10:55:59 +0100

    feat: show pail positions

--- src/components/PAIL/Owned.tsx
+++ src/components/PAIL/Owned.tsx
@@ -8,7 +8,57 @@ import {
 import { PAIL_ADDRESS, PAIL_NFT_ADDRESS } from "../../constants/amm";
 import toast from "react-hot-toast";
 import { SecondaryConnectWallet } from "../ConnectWallet/Button";
-import { Button, H5 } from "../common";
+import { Button, Divider, H5, MaturityStacked, P3, P4 } from "../common";
+import { usePailTokenInfo } from "../../hooks/usePailInfo";
+import { Token } from "../../classes/Token";
+import { PairNameAboveBadge } from "../TokenBadge";
+
+const PailItem = ({
+  id,
+  handleClick,
+}: {
+  id: number;
+  handleClick: (
+    tokenId: number,
+    funcName: "hedge_close" | "hedge_settle"
+  ) => void;
+}) => {
+  const { isLoading, isError, data } = usePailTokenInfo(id);
+
+  if (isLoading) {
+    return <P4>Loading...</P4>;
+  }
+  if (isError || !data) {
+    return <P4>Error</P4>;
+  }
+
+  const base = Token.byAddress(data.base);
+  const quote = Token.byAddress(data.quote);
+  const maturity = Number(data.maturity);
+
+  const isExpired = maturity * 1000 < Date.now();
+  const funcName = isExpired ? "hedge_settle" : "hedge_close";
+
+  return (
+    <div className="flex justify-between my-2 py-3 text-left w-[400px]">
+      <div className="w-full">
+        <PairNameAboveBadge tokenA={base} tokenB={quote} />
+      </div>
+      <div className="w-full">
+        <MaturityStacked timestamp={maturity} />
+      </div>
+      <div className="w-full">
+        <Button
+          type="primary"
+          className="w-full"
+          onClick={() => handleClick(id, funcName)}
+        >
+          {isExpired ? "Settle" : "Close"}
+        </Button>
+      </div>
+    </div>
+  );
+};
 
 export const Owned = () => {
   const { address } = useAccount();
@@ -62,37 +112,56 @@ export const Owned = () => {
   return (
     <div className="flex flex-col">
       <H5>Live</H5>
-      {data?.live?.length === 0 ? (
-        <p>No live PAIL</p>
-      ) : (
-        data.live.map((id, i) => (
-          <div key={i} className="flex items-center gap-4">
-            <p>token id {id}</p>
-            <Button
-              type="primary"
-              onClick={() => handleFinalise(id, "hedge_close")}
-            >
-              Close
-            </Button>
+      <div className="overflow-x-auto">
+        <div className="flex flex-col g-3 w-[400px]">
+          <div className="flex justify-between my-2 py-3 border-dark-tertiary border-y-[0.5px] text-left w-[400px]">
+            <div className="w-full">
+              <P4 className="text-dark-secondary">PAIR</P4>
+            </div>
+            <div className="w-full">
+              <P4 className="text-dark-secondary">MATURITY</P4>
+            </div>
+            {/* Empty room for button */}
+            <div className="w-full" />
           </div>
-        ))
-      )}
+          {data?.live?.length === 0 ? (
+            <div className="my-2 py-3 max-w-big">
+              <P3 className="font-semibold text-center">Nothing to show</P3>
+            </div>
+          ) : (
+            data?.live?.map((id, i) => (
+              <PailItem key={i} id={id} handleClick={handleFinalise} />
+            ))
+          )}
+        </div>
+      </div>
+
+      <Divider className="my-8" />
+
       <H5>Expired</H5>
-      {data?.expired?.length === 0 ? (
-        <p>No expired PAIL</p>
-      ) : (
-        data.expired.map((id, i) => (
-          <div key={i} className="flex items-center gap-4">
-            <p>token id {id}</p>
-            <Button
-              onClick={() => handleFinalise(id, "hedge_settle")}
-              type="primary"
-            >
-              Settle
-            </Button>
+      <div className="overflow-x-auto">
+        <div className="flex flex-col g-3 w-[400px]">
+          <div className="flex justify-between my-2 py-3 border-dark-tertiary border-y-[0.5px] text-left w-[400px]">
+            <div className="w-full">
+              <P4 className="text-dark-secondary">PAIR</P4>
+            </div>
+            <div className="w-full">
+              <P4 className="text-dark-secondary">MATURITY</P4>
+            </div>
+            {/* Empty room for button */}
+            <div className="w-full" />
           </div>
-        ))
-      )}
+          {data?.expired?.length === 0 ? (
+            <div className="my-2 py-3 max-w-big">
+              <P3 className="font-semibold text-center">Nothing to show</P3>
+            </div>
+          ) : (
+            data?.expired?.map((id, i) => (
+              <PailItem key={i} id={id} handleClick={handleFinalise} />
+            ))
+          )}
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/hooks/usePailInfo.ts
@@ -0,0 +1,107 @@
+import { useReadContract } from "@starknet-react/core";
+import { PAIL_NFT_ADDRESS } from "../constants/amm";
+
+export const usePailTokenInfo = (id: number) => {
+  const {
+    data: maturity,
+    isLoading: maturityLoading,
+    isError: maturityError,
+    error: matError,
+  } = useReadContract({
+    abi: [
+      {
+        name: "maturity",
+        type: "function",
+        inputs: [
+          {
+            name: "token_id",
+            type: "core::integer::u256",
+          },
+        ],
+        outputs: [
+          {
+            type: "core::integer::u64",
+          },
+        ],
+        state_mutability: "view",
+      },
+    ] as const,
+    functionName: "maturity",
+    address: PAIL_NFT_ADDRESS as `0x${string}`,
+    args: [id],
+  });
+
+  const {
+    data: base,
+    isLoading: baseLoading,
+    isError: baseError,
+    error: baError,
+  } = useReadContract({
+    abi: [
+      {
+        name: "base_token_address",
+        type: "function",
+        inputs: [
+          {
+            name: "token_id",
+            type: "core::integer::u256",
+          },
+        ],
+        outputs: [
+          {
+            type: "core::starknet::contract_address::ContractAddress",
+          },
+        ],
+        state_mutability: "view",
+      },
+    ] as const,
+    functionName: "base_token_address",
+    address: PAIL_NFT_ADDRESS as `0x${string}`,
+    args: [id],
+  });
+
+  const {
+    data: quote,
+    isLoading: quoteLoading,
+    isError: quoteError,
+    error: qoError,
+  } = useReadContract({
+    abi: [
+      {
+        name: "quote_token_address",
+        type: "function",
+        inputs: [
+          {
+            name: "token_id",
+            type: "core::integer::u256",
+          },
+        ],
+        outputs: [
+          {
+            type: "core::starknet::contract_address::ContractAddress",
+          },
+        ],
+        state_mutability: "view",
+      },
+    ] as const,
+    functionName: "quote_token_address",
+    address: PAIL_NFT_ADDRESS as `0x${string}`,
+    args: [id],
+  });
+
+  console.log({ matError, baError, qoError });
+
+  const isError = maturityError || baseError || quoteError;
+  const isLoading =
+    !isError && (maturityLoading || baseLoading || quoteLoading);
+
+  if (isError || isLoading) {
+    return { data: undefined, isError, isLoading };
+  }
+
+  return {
+    data: { quote, base, maturity },
+    isLoading,
+    isError,
+  };
+};
