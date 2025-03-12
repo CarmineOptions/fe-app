856a865a218f9929171c23da89987fb5285d6ea9
DaveVodrazka <dave.vodrazka@gmail.com>
2025-03-12 23:04:21 +0100

    fix: use new pail token endpoint

--- src/components/PAIL/Owned.tsx
+++ src/components/PAIL/Owned.tsx
@@ -1,5 +1,5 @@
 import { LoadingAnimation } from "../Loading/Loading";
-import { usePailTokenIds } from "../../hooks/usePailTokenIds";
+import { PailTokenDesc, usePailTokens } from "../../hooks/usePailTokenIds";
 import {
   useAccount,
   useProvider,
@@ -8,30 +8,24 @@ import {
 import { PAIL_ADDRESS, PAIL_NFT_ADDRESS } from "../../constants/amm";
 import toast from "react-hot-toast";
 import { SecondaryConnectWallet } from "../ConnectWallet/Button";
-import { Button, Divider, H5, MaturityStacked, P3, P4 } from "../common";
-import { usePailTokenInfo } from "../../hooks/usePailInfo";
+import { Button, H4, MaturityStacked, P3 } from "../common";
 import { PairNameAboveBadge } from "../TokenBadge";
+import { useState } from "react";
+import { Token } from "../../classes/Token";
 
-const PailItem = ({
-  id,
-  handleClick,
-}: {
-  id: number;
+type PailItemProps = {
+  pail: PailTokenDesc;
   handleClick: (
     tokenId: number,
     funcName: "hedge_close" | "hedge_settle"
   ) => void;
-}) => {
-  const { isLoading, isError, data } = usePailTokenInfo(id);
+};
 
-  if (isLoading) {
-    return <P4>Loading...</P4>;
-  }
-  if (isError || !data) {
-    return <P4>Error</P4>;
-  }
+const PailItem = ({ pail, handleClick }: PailItemProps) => {
+  const { base, quote, maturity, id } = pail;
 
-  const { base, quote, maturity } = data;
+  const baseToken = Token.byAddress(base);
+  const quoteToken = Token.byAddress(quote);
 
   const isExpired = maturity * 1000 < Date.now();
   const funcName = isExpired ? "hedge_settle" : "hedge_close";
@@ -39,7 +33,7 @@ const PailItem = ({
   return (
     <div className="flex justify-between my-2 py-3 text-left w-[400px]">
       <div className="w-full">
-        <PairNameAboveBadge tokenA={base} tokenB={quote} />
+        <PairNameAboveBadge tokenA={baseToken} tokenB={quoteToken} />
       </div>
       <div className="w-full">
         <MaturityStacked timestamp={maturity} />
@@ -59,9 +53,10 @@ const PailItem = ({
 
 export const Owned = () => {
   const { address } = useAccount();
-  const { isLoading, isError, error, data } = usePailTokenIds();
+  const { isLoading, isError, error, data } = usePailTokens();
   const { sendAsync } = useSendTransaction({});
   const { provider } = useProvider();
+  const [state, setState] = useState<"live" | "expired">("live");
 
   if (!address) {
     return (
@@ -106,59 +101,47 @@ export const Owned = () => {
       });
   };
 
+  const now = new Date().getTime() / 1000;
+
+  const hedges =
+    state === "live"
+      ? data.filter((hedge) => hedge.maturity > now)
+      : data.filter((hedge) => hedge.maturity <= now);
+
   return (
-    <div className="flex flex-col">
-      <H5>Live</H5>
-      <div className="overflow-x-auto">
-        <div className="flex flex-col g-3 w-[400px]">
-          <div className="flex justify-between my-2 py-3 border-dark-tertiary border-y-[0.5px] text-left w-[400px]">
-            <div className="w-full">
-              <P4 className="text-dark-secondary">PAIR</P4>
-            </div>
-            <div className="w-full">
-              <P4 className="text-dark-secondary">MATURITY</P4>
-            </div>
-            {/* Empty room for button */}
-            <div className="w-full" />
+    <div>
+      <div className="flex flex-col md:flex-row items-center gap-7 mb-6">
+        <H4>Hedges</H4>
+        <div className="flex gap-1">
+          <div>
+            <Button
+              type={state === "live" ? "primary" : "secondary"}
+              outlined={state !== "live"}
+              onClick={() => setState("live")}
+            >
+              live
+            </Button>
           </div>
-          {data?.live?.length === 0 ? (
-            <div className="my-2 py-3 max-w-big">
-              <P3 className="font-semibold text-center">Nothing to show</P3>
-            </div>
-          ) : (
-            data?.live?.map((id, i) => (
-              <PailItem key={i} id={id} handleClick={handleFinalise} />
-            ))
-          )}
+          <div>
+            <Button
+              type={state === "expired" ? "primary" : "secondary"}
+              outlined={state !== "expired"}
+              onClick={() => setState("expired")}
+            >
+              expired
+            </Button>
+          </div>
         </div>
       </div>
-
-      <Divider className="my-8" />
-
-      <H5>Expired</H5>
-      <div className="overflow-x-auto">
-        <div className="flex flex-col g-3 w-[400px]">
-          <div className="flex justify-between my-2 py-3 border-dark-tertiary border-y-[0.5px] text-left w-[400px]">
-            <div className="w-full">
-              <P4 className="text-dark-secondary">PAIR</P4>
-            </div>
-            <div className="w-full">
-              <P4 className="text-dark-secondary">MATURITY</P4>
-            </div>
-            {/* Empty room for button */}
-            <div className="w-full" />
-          </div>
-          {data?.expired?.length === 0 ? (
-            <div className="my-2 py-3 max-w-big">
-              <P3 className="font-semibold text-center">Nothing to show</P3>
-            </div>
-          ) : (
-            data?.expired?.map((id, i) => (
-              <PailItem key={i} id={id} handleClick={handleFinalise} />
-            ))
-          )}
+      {hedges.length === 0 ? (
+        <P3>Nothing to show</P3>
+      ) : (
+        <div>
+          {hedges.map((pail, i) => (
+            <PailItem pail={pail} handleClick={handleFinalise} key={i} />
+          ))}
         </div>
-      </div>
+      )}
     </div>
   );
 };
