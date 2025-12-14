f57ed188951cc4287e42c7cf8ae37966f9249230
DaveVodrazka <dave.vodrazka@gmail.com>
2025-12-14 01:12:43 +0100

    feat: rework user points

--- src/components/Points/PointsLeaderboard.tsx
+++ src/components/Points/PointsLeaderboard.tsx
@@ -1,57 +1,40 @@
-import { useQuery } from "@tanstack/react-query";
-import { QueryKeys } from "../../queries/keys";
-import {
-  fetchBraavosBonus,
-  fetchTopUserPoints,
-  fetchUserPoints,
-} from "./fetch";
 import { LoadingAnimation } from "../Loading/Loading";
 import { formatNumber, standardiseAddress } from "../../utils/utils";
 import { useAccount } from "@starknet-react/core";
 import { useDomains } from "../../hooks/useDomains";
 import { useDomain } from "../../hooks/useDomain";
 import { Leaderboard } from "../Leaderboard";
+import { useUserPoints } from "../../hooks/useUserPoints";
+import { UserPoints } from "carmine-sdk/api";
 
 const parseData = ({
-  trading_points,
-  liquidity_points,
-  referral_points,
-  vote_points,
-  total_points,
-}: {
-  trading_points: number;
-  liquidity_points: number;
-  referral_points: number;
-  vote_points: number;
-  total_points: number;
-}): [JSX.Element, JSX.Element, JSX.Element, JSX.Element, JSX.Element] => {
+  tradePoints,
+  liquidityPoints,
+  referralPoints,
+  votePoints,
+  totalPoints,
+}: Partial<UserPoints>): [
+  JSX.Element,
+  JSX.Element,
+  JSX.Element,
+  JSX.Element,
+  JSX.Element
+] => {
   return [
-    <p>{formatNumber(trading_points)}</p>,
-    <p>{formatNumber(liquidity_points)}</p>,
-    <p>{formatNumber(referral_points)}</p>,
-    <p>{formatNumber(vote_points)}</p>,
-    <p>{formatNumber(total_points)}</p>,
+    <p>{formatNumber(tradePoints!)}</p>,
+    <p>{formatNumber(liquidityPoints!)}</p>,
+    <p>{formatNumber(referralPoints!)}</p>,
+    <p>{formatNumber(votePoints!)}</p>,
+    <p>{formatNumber(totalPoints!)}</p>,
   ];
 };
 
 export const PointsLeaderboard = () => {
   const { address } = useAccount();
-  const { isLoading, isError, data } = useQuery({
-    queryKey: [QueryKeys.topUserPoints],
-    queryFn: fetchTopUserPoints,
-  });
-  const { data: braavosData } = useQuery({
-    queryKey: [QueryKeys.braavosBonus],
-    queryFn: fetchBraavosBonus,
-  });
-  const { data: currentUser } = useQuery({
-    queryKey: [QueryKeys.userPoints, address],
-    queryFn: async () => fetchUserPoints(address!),
-    enabled: !!address,
-  });
+  const { isLoading, isError, data } = useUserPoints(address);
   const { username } = useDomain(address);
   const addresses = data
-    ? data.map((p) => standardiseAddress(p.address))
+    ? data.top.map((p) => standardiseAddress(p.userAddress))
     : undefined;
   const { domains } = useDomains(addresses);
 
@@ -63,25 +46,26 @@ export const PointsLeaderboard = () => {
     return <p>Error</p>;
   }
 
-  const items = data.map(({ address, position, ...rest }) => {
+  const items = data.top.map(({ userAddress, position, ...rest }) => {
     const username = domains
       ? domains.find(
-          (d) => standardiseAddress(d.address) === standardiseAddress(address)
+          (d) =>
+            standardiseAddress(d.address) === standardiseAddress(userAddress)
         )?.domain
       : undefined;
     return {
       position,
-      address,
+      address: userAddress,
       username,
       data: parseData(rest),
     };
   });
 
-  const user = currentUser && {
-    position: currentUser.position,
-    address: currentUser.address,
+  const user = data.user && {
+    position: data.user.position,
+    address: data.user.userAddress,
     username: username ? username : undefined,
-    data: parseData(currentUser),
+    data: parseData(data.user),
   };
 
   const header = [
@@ -99,7 +83,7 @@ export const PointsLeaderboard = () => {
       header={header}
       items={items}
       user={user}
-      braavos={braavosData}
+      // braavos={braavosData}
       name="points"
     />
   );
---
+++ src/hooks/useUserPoints.ts
@@ -0,0 +1,8 @@
+import { CarmineApi, TopUsers } from "carmine-sdk/api";
+import { useQuery } from "@tanstack/react-query";
+
+export const useUserPoints = (user?: string) =>
+  useQuery<TopUsers, Error>({
+    queryKey: ["user-points", user],
+    queryFn: () => CarmineApi.userPoints(user),
+  });
